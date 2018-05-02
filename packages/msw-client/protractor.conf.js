1cae1766a5e587227509ceda76ffbde34e7519a3
dkhnn <dk080e@intl.att.com>
2018-05-03 00:08:38 +0300

    :green_heart: another fix

--- packages/msw-client/protractor.conf.js
+++ packages/msw-client/protractor.conf.js
@@ -11,9 +11,6 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome'
   },
-  chromeOptions: {
-  args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
-  },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
